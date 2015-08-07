c5bf86d336e488ffc153d002eca1d995c00a9f48
michaelzoidl <michael@zoidl.de>
2015-08-07 15:22:56 +0200

    Add hasTildeInString function TDD

--- plugin/index.js
+++ plugin/index.js
@@ -1,15 +1,35 @@
+import path from 'path';
+import root from 'app-root-path';
+
 export default function create(babel) {
 
   class BabelRootImport {
 
-    foo = 'asd';
+    root = root.path;
 
-    constructor() {
-      console.log('constructor');
+    constructor(babel) {
+      // return new babel.Transformer("babel-plugin-project-relative-require", {
+      //   ImportDeclaration: function(node, parent) {
+      //
+      //   };
+      // };
     }
 
     transformRelativeToRootImport() {
       return '';
+    }
+
+    hasTildeInString(string) {
+      let containsTilde = false;
+
+      if(typeof string === 'string') {
+        const firstTwoCharactersOfString = string.substring(0, 2);
+        if(firstTwoCharactersOfString === '~/') {
+          containsTilde = true;
+        }
+      }
+
+      return containsTilde;
     }
   }
 
--- test/plugin.spec.js
+++ test/plugin.spec.js
@@ -8,10 +8,38 @@ describe('Babel Root Import', () => {
     plugin = new babelRootImportPlugin();
   });
 
+  describe('Vars', () => {
+    it('returns the root path', () => {
+      const rootByProcess = process.cwd();
+      expect(plugin.root).to.equal(rootByProcess);
+    });
+  });
+
   describe('transformRelativeToRootImport', () => {
     it('returns a string', () => {
       const func = plugin.transformRelativeToRootImport();
       expect(func).to.be.a('string');
+    });
+  });
+
+  describe('hasTildeInString', () => {
+    it('returns a boolean', () => {
+      const func = plugin.hasTildeInString();
+      expect(func).to.be.a('boolean');
+    });
+
+    it('check if a "~/" is at the beginning of the string', () => {
+      const withoutTilde = plugin.hasTildeInString('some/path');
+      const withTilde = plugin.hasTildeInString('~/some/path');
+      expect(withoutTilde).to.be.false;
+      expect(withTilde).to.be.true;
+    });
+
+    it('returns false if no string is passed', () => {
+      const nothingPassed = plugin.hasTildeInString();
+      const wrongTypePassed = plugin.hasTildeInString([]);
+      expect(nothingPassed).to.be.false;
+      expect(wrongTypePassed).to.be.false;
     });
   });
 });
