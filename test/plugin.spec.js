ee9d5cbe9cf12673fc7ff5642761e4c56ac96fee
michaelzoidl <michael@zoidl.de>
2015-11-15 17:10:45 +0100

    Add integration test

--- test/plugin.spec.js
+++ test/plugin.spec.js
@@ -1,4 +1,5 @@
 import BabelRootImportPlugin from '../plugin';
+import * as babel from 'babel-core';
 
 describe('Babel Root Import', () => {
   let babelRootImportPlugin, plugin;
@@ -51,6 +52,17 @@ describe('Babel Root Import', () => {
       const wrongTypePassed = plugin.hasTildeInString([]);
       expect(nothingPassed).to.be.false;
       expect(wrongTypePassed).to.be.false;
+    });
+  });
+
+  describe('Babel Plugin', () => {
+    it('transforms the relative path into an absolute path', () => {
+      const targetRequire = `${process.cwd()}/some/example.js`;
+      const transformedCode = babel.transform("import SomeExample from '~/some/example.js';", {
+        plugins: [BabelRootImportPlugin]
+      });
+
+      expect(transformedCode.code).to.contain(targetRequire);
     });
   });
 });
