ca87b80463e47ff57c60958a9800872776cf85a9
michaelzoidl <michael@zoidl.de>
2015-11-30 13:25:28 +0100

    Updated babel plugin api and splitted plugin and helper in 2 files

---
+++ plugin/helper.js
@@ -0,0 +1,32 @@
+export default function(path) {
+  class BabelRootImportHelper {
+
+    root = global.rootPath || process.cwd()
+
+    transformRelativeToRootPath(path) {
+      if (this.hasTildeInString(path)) {
+        const withoutTilde = path.substring(2, path.length);
+        return `${this.root}/${withoutTilde}`;
+      }
+      if (typeof path === 'string') {
+        return path;
+      }
+      throw new Error('ERROR: No path passed');
+    }
+
+    hasTildeInString(string) {
+      let containsTilde = false;
+
+      if (typeof string === 'string') {
+        const firstTwoCharactersOfString = string.substring(0, 2);
+        if (firstTwoCharactersOfString === '~/') {
+          containsTilde = true;
+        }
+      }
+
+      return containsTilde;
+    }
+  }
+
+  return new BabelRootImportHelper();
+}
--- plugin/index.js
+++ plugin/index.js
@@ -1,46 +1,19 @@
-export default function create(babel) {
+import BabelRootImportHelper from './helper';
+
+export default function({ types: t }) {
   class BabelRootImport {
-
-    root = global.rootPath || process.cwd()
-
     constructor() {
-      if (babel) {
-        return new babel.Transformer('babel-root-import', {
-          ImportDeclaration: (node) => {
-            const givenPath = node.source.value;
-
-            if (this.hasTildeInString(givenPath)) {
-              node.source.value = this.transformRelativeToRootPath(node.source.value);
+      const that = this;
+      return {
+        visitor: {
+          ImportDeclaration(path) {
+            const givenPath = path.node.source.value;
+            if(BabelRootImportHelper().hasTildeInString(givenPath)) {
+              path.node.source.value = BabelRootImportHelper().transformRelativeToRootPath(path.node.source.value);
             }
-
-            return node;
           }
-        });
-      }
-    }
-
-    transformRelativeToRootPath(path) {
-      if (this.hasTildeInString(path)) {
-        const withoutTilde = path.substring(2, path.length);
-        return `${this.root}/${withoutTilde}`;
-      }
-      if (typeof path === 'string') {
-        return path;
-      }
-      throw new Error('ERROR: No path passed');
-    }
-
-    hasTildeInString(string) {
-      let containsTilde = false;
-
-      if (typeof string === 'string') {
-        const firstTwoCharactersOfString = string.substring(0, 2);
-        if (firstTwoCharactersOfString === '~/') {
-          containsTilde = true;
         }
-      }
-
-      return containsTilde;
+      };
     }
   }
 
